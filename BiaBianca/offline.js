3777014b6c677b5ee49263b13bbbbdb9691a9d2c
M. A <6686318+marcoaureliom@users.noreply.github.com>
2021-03-06 13:49:43 -0300

    n j

---
+++ BiaBianca/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1597952867,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backgroundtile.png",
+		"images/player-sheet0.png",
+		"images/solidtile.png",
+		"images/jumpthrutile.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/spritefont.png",
+		"images/sprite4-sheet0.png",
+		"media/342751__rhodesmas__coins-purchase-3.m4a",
+		"media/342751__rhodesmas__coins-purchase-3.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
